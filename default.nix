95471d0dfcd0e00ba6adcafb8db66e8e99c6f238
Ezekiel Enns <Ezekiel.Enns@protonmail.com>
2024-01-15 22:20:00 -0400

    init

---
+++ default.nix
@@ -0,0 +1,11 @@
+{ pkgs ? import <nixpkgs> {
+  overlays = [
+    (import (builtins.fetchTarball
+      "https://github.com/oxalica/rust-overlay/archive/master.tar.gz"))
+  ];
+} }:
+with pkgs;
+let rust = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+in pkgs.mkShell { packages = [ 
+    rust rust-analyzer cargo-generate nodejs_latest wasm-pack  ]; }
+#cargo b --manifest-path=logic/Cargo.toml

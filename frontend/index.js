e70436baf8bb0e97165038b281faa112e76112ff
Ezekiel Enns <Ezekiel.Enns@protonmail.com>
2024-02-02 14:07:28 -0400

    got things setup gotta upgrade to vite....

--- frontend/index.js
+++ frontend/index.js
@@ -1,44 +1,45 @@
-
 import * as sim from "./dist/wasm32-unknown-unknown/debug/simulation.wasm"
 import bencode from 'bencode'
+import { init } from "glyplib";
 
-var/** @type {number} */ ptr,
-   /** @type {number} */ len;
+var/** @type {number} */ ptr = sim.get_buffs(),
+   /** @type {number} */ len = sim.get_len(),
+   /** @type {HTMLCanvasElement} */ cnv = document.getElementById("canvas")
 
-/**
- * @type {WebAssembly.Memory}
- */
-const memeory = sim.memory;
-ptr = sim.get_buffs()
-console.log(memeory)
-// len = new Uint8Array(memeory.buffer,ptr,1);
-// console.log(len[0])
-let buff = new Uint8Array(memeory.buffer,ptr,sim.get_len());
-console.log(buff)
-let render_data = bencode.decode(buff);
-console.log(render_data)
-console.log(String.fromCharCode(render_data.actors.textures[0]))
-
-// sim.update();
-// ptr = sim.get_buffs(ptr,len);
-// len = sim.get_buff_len();
-// buff = new Uint8Array(memeory.buffer,ptr,len);
-// render_data = bencode.decode(buff);
-//
-// /** @type {number} */
-// ptr = sim.test()
-// console.log(ptr)
-// let ptrs = new Uint8Array(memeory.buffer,ptr,3)
-// console.log(ptrs)
-//let t = new TextDecoder()
-//console.log(ptrs)
+/** @type {WebAssembly.Memory} */
+var memeory = sim.memory;
 
 /**
- * 
+ * @typedef {Object} RenderBuffers
+ * @property {Uint8Array} textures
+ * @property {Uint8Array} colors
+ * @property {Uint8Array} locations
+ * @property {number} len
+ * @typedef {Object} RenderDataRaw
+ * @property {RenderBuffers} actors
+ * @property {RenderBuffers} items
+ * @property {RenderBuffers} tiles
+ *
+ * @returns {RenderDataRaw} rd
  */
 function getRenderData(){
-    //do whats on the top and just return that 
+    ptr = sim.get_buffs(ptr,len)
+    let buff = new Uint8Array(memeory.buffer,ptr,sim.get_len())
+    return bencode.decode(buff)
 }
+//render TODO move init to be higher up/happen automatically
+(async ()=> { 
+    //inits the layers and webgl
+    await init(cnv,"monogram.ttf")
+    console.log("hi")
+    //https://developer.mozilla.org/en-US/docs/Web/API/window/requestAnimationFrame
+    function renderLoop() {
+        requestAnimationFrame(renderLoop)
+    }
+    requestAnimationFrame(renderLoop)
+})()
+
+
 //TODO bind movment  
 
 //TODO make render loop like in testing for glyph lib
